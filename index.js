69c82c3921b92a2a8252431ce21c24a0187f702a
stuarts-exmos <86110417+stuarts-exmos@users.noreply.github.com>
2022-05-05 23:18:09 +0100

    Using a different library to send to rabbit

--- index.js
+++ index.js
@@ -1,41 +1,31 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const amqplib = require('amqplib');
+const amqp = require('@cloudamqp/amqp-client');
 
 // Get the input to the action
-// const rmqPassword = core.getInput("rmqPassword");
-// const rmqUsername = core.getInput("rmqUsername");
-// const rmqHost = core.getInput("rmqHost");
-// const rmqVirtualHost = core.getInput("rmqVirtualHost");
-// const rmqExchange = core.getInput("rmqExchange");
-// const rmqRoutingKey = core.getInput("routingKey");
-// const message = core.getInput("message");
+const rmqPassword = core.getInput("rmqPassword");
+const rmqUsername = core.getInput("rmqUsername");
+const rmqHost = core.getInput("rmqHost");
+const rmqVirtualHost = core.getInput("rmqVirtualHost");
+const rmqExchange = core.getInput("rmqExchange");
+const rmqRoutingKey = core.getInput("routingKey");
+const message = core.getInput("message");
 
-const rmqPassword = "Thg5WGy9FsWrWQ7";
-const rmqUsername = "githubactions";
-const rmqHost = "cyan-quail.rmq.cloudamqp.com";
-const rmqVirtualHost = "egiogzlg";
-const rmqExchange = "egiogzlg";
-const rmqRoutingKey = "T.0.F.Github.C.[GithHubRelease]";
-const message = JSON.stringify({msg: "github released foobar", app: "foobar", version: "1.0.0.0"});
-
-
-async function produce(){
+async function run() {
     try {
-        console.log("here");
-        const url = "amqps://" + rmqUsername + ":" + rmqPassword + "@" + rmqHost + "/" + rmqVirtualHost
-        const json = JSON.stringify(message);
-        console.log("json'ized the message")
-        var conn = await amqplib.connect(url);
-        var ch = await conn.createChannel();
-        await ch.assertExchange(rmqExchange, 'topic', {durable: true});
-        await ch.publish(rmqExchange, rmqRoutingKey, Buffer.from(json));
-        await conn.close()
-    } catch(error){
-        console.log("error: " + error.message);
-        core.setFailed(error.message)
+      const url = "amqps://" + rmqUsername + ":" + rmqPassword + "@" + rmqHost + "/" + rmqVirtualHost
+      var amqpClient = new amqp.AMQPClient(url);
+      const conn = await amqpClient.connect();
+      const ch = await conn.channel();
+      console.log("got channel");
+      await ch.basicPublish(rmqExchange, rmqRoutingKey, Buffer.from(message));
+      await ch.close();
+      await conn.close();
+    } catch (e) {
+      console.error("ERROR", e);
+      e.connection.close();
+      core.setFailed(error.message)
     }
-}
-
-produce();
-
+  }
+  
+  run()
